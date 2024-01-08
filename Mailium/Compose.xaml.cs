a61a55c75a297f5043da998f224ee42c593cc856
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2024-01-08 19:18:39 +0100

    Added sending mail on server

--- Mailium/Compose.xaml.cs
+++ Mailium/Compose.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,9 +27,48 @@ namespace Mailium
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            string recipient = txtRecipient.Text;
+            string subject = txtSubject.Text;
+            string content = txtContent.Text;
 
+            if (string.IsNullOrWhiteSpace(recipient) || string.IsNullOrWhiteSpace(subject) || string.IsNullOrWhiteSpace(content))
+            {
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            await SendEmail(recipient, subject, content);
+        }
+
+        private async Task SendEmail(string recipient, string subject, string content)
+        {
+            //TODO: KRIPTIRAJ
+            string encryptedContent = content;
+            string encryptedSubject = subject;
+            int senderId = UserManager.GetCurrentUser().Id;
+
+            var messageDTO = new { senderId = senderId, receiverEmail = recipient, title = encryptedSubject, content = encryptedContent, sentAt = DateTime.Now.ToString() };
+
+            var json = JsonConvert.SerializeObject(messageDTO);
+            var mailContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost:5294/Message/send", mailContent);
+                if(response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Poruka poslana!");
+                    txtRecipient.Clear();
+                    txtSubject.Clear();
+                    txtContent.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Poruka nije poslana!");
+                }
+            }
         }
     }
 }
