5096dc064c2ee944340653704824e3336345bf62
Karlo Vardijan <kvardijan20@foi.hr>
2024-01-08 13:56:14 +0100

    Installed NewtonsoftJson package. Implemented login fetch.

--- Mailium/Login.xaml.cs
+++ Mailium/Login.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,50 @@ namespace Mailium
     /// </summary>
     public partial class Login : UserControl
     {
+        private const string BaseUrl = "http://localhost:5294";
+        private static readonly HttpClient client = new HttpClient();
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateForm())
+            {
+                SendLoginRequestAsync();
+            }
+        }
+
+        private async void SendLoginRequestAsync()
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+            var body = new { email = email, password = password };
+            string bodyJson = JsonConvert.SerializeObject(body);
+            var parameters = new StringContent(bodyJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{BaseUrl}/Auth/login", parameters);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("Login success.");
+                //open inbox
+            }
+            else
+            {
+                MessageBox.Show("Login failed." + response.ReasonPhrase.ToString());
+            }
+        }
+
+        private bool ValidateForm()
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("One or both of the fields are empty.");
+                return false;
+            }
+            return true;
         }
     }
 }
